ba8fac18d03453b39ba1c775f5a449639555c52f
Tohna <kaptsan.tal@gmail.com>
2018-05-06 17:04:56 +0300

    fixed photo gallery depend on screen-width

--- component/price/price.js
+++ component/price/price.js
@@ -5,7 +5,7 @@ var app = angular.module('price',[]);
 app.component('price',{
     templateUrl:'component/price/price.html',
     controller: function($scope){
-        $scope.numOfPeople = ["עד 10 אנשים", "עד 15 אנשים", "עד 20 אנשים","יותר מ 20 אנשים"];
+        $scope.numOfPeople = ["עד 10 אנשים", "מ 10-15 אנשים", "מ 16-20 אנשים","מ 21-25 אנשים"];
         $scope.costs = ["80","70","60","50", "50"]
     }
 })
--- js/custom.js
+++ js/custom.js
@@ -11,10 +11,50 @@ $(document).ready(function () {
         $('.navigation').click(function(){
             $('.navigation').css('display','none');
         })
-    
+        var mySwiper = new Swiper('.swiper-container', {
+            // Optional parameters
+            loop: true,
+            slidesPerView: 3,
+            spaceBetween: 10,
+            autoplay: {
+                delay: 5000
+            },
+
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+            },
+
+            // Navigation arrows
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+
+        })
     }
-    else if(widthScreen > 760){
-        
+    else if(widthScreen <= 320){
+            var mySwiper = new Swiper('.swiper-container', {
+                // Optional parameters
+                loop: true,
+                slidesPerView: 1,
+                spaceBetween: 10,
+                autoplay: {
+                    delay: 5000
+                },
+
+                // If we need pagination
+                pagination: {
+                    el: '.swiper-pagination',
+                },
+
+                // Navigation arrows
+                navigation: {
+                    nextEl: '.swiper-button-next',
+                    prevEl: '.swiper-button-prev',
+                },
+
+            })
         }
     window.onload = function () {
             // solve the problem when refresh page the active page will be always the home page 
